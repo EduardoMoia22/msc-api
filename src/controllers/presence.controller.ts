3dad3927c025e8dae64123f7e48534721f3ab4d2
EduardoMoia22 <eduardomoiasantos@gmail.com>
2024-09-25 15:19:33 -0300

    added attendance listing feature with optional filters

--- src/controllers/presence.controller.ts
+++ src/controllers/presence.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { PresenceRequestDTO, PresenceResponseDTO } from "src/DTOs/presence.dtos";
 import { Presence } from "src/entities/presence.entity";
 import { AuthGuard } from "src/guards/auth.guard";
@@ -23,5 +23,32 @@ export class PresenceController {
         const presence: Presence = await this.presenceService.createPresence(body);
 
         return PresenceResponseDTO.fromEntity(presence);
+    }
+
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Buscar presenças, contendo alguns filtros opcionais' })
+    @ApiResponse({ status: 200, description: 'Ok', type: PresenceResponseDTO, isArray: true })
+    @ApiResponse({ status: 400, description: 'Bad Request.' })
+    @ApiResponse({ status: 401, description: 'Unauthorized.' })
+    @ApiResponse({ status: 404, description: 'Not Found.' })
+    @ApiQuery({ name: 'studentId', required: false, type: String, description: 'Id do aluno' })
+    @ApiQuery({ name: 'teacherId', required: false, type: String, description: 'Id do professor' })
+    @ApiQuery({ name: 'startDate', required: false, type: String, description: 'Data inicial para o filtro (formato YYYY-MM-DD)' })
+    @ApiQuery({ name: 'endDate', required: false, type: String, description: 'Data final para o filtro (formato YYYY-MM-DD)' })
+    @Get()
+    public async listPresences(
+        @Query("studentId") studentId?: string,
+        @Query("teacherId") teacherId?: string,
+        @Query("startDate") startDate?: string,
+        @Query("endDate") endDate?: string
+    ): Promise<PresenceResponseDTO[]> {
+        const presences: Presence[] = await this.presenceService.listPresences(
+            parseInt(studentId),
+            parseInt(teacherId),
+            startDate,
+            endDate);
+
+        return presences.map(PresenceResponseDTO.fromEntity);
     }
 }
