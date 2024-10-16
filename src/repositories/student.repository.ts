9d458d4cff35ee783d5e9c8f15ad6ce78db57749
EduardoMoia22 <eduardomoiasantos@gmail.com>
2024-10-16 11:10:09 -0300

    hotfix/import student mapper error

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -1,7 +1,7 @@
 import { PrismaService } from "src/configs/prisma.service";
 import { Injectable } from "@nestjs/common";
 import { Student } from "src/entities/student.entity";
-import { StudentMapper } from "src/mappers/Student.mapper";
+import { StudentMapper } from "src/mappers/student.mapper";
 
 @Injectable()
 export class StudentRepository {
